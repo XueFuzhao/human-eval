a553d3989518abad86da457f8ffd82d858640e40
Xue Fuzhao <xuefuzhao@outlook.com>
2023-08-29 00:35:22 +0800

    Update

--- utils/convert.py
+++ utils/convert.py
@@ -17,9 +17,36 @@ with open(input_file_path, 'r') as input_file:
         example = json.loads(line.strip())
         completion = example['output'].replace('<extra_id_0>', '')
 
+        completion = completion.replace("\t", "    ")
+        completion = completion.lstrip("\n")
+        completion = completion.split("\n\n")[0]
+        completion = completion.replace("\r", "")
+        if "```python" in completion:
+            def_line = completion.index("```python")
+            completion = completion[def_line:].strip()
+            completion = completion.replace("```python", "")
+            # print(completion)
+            try:
+                next_line = completion.index("```")
+                completion = completion[:next_line].strip()
+            except:
+                print(completion)
+                print("================\n")
+            # print(completion)
+        if '__name__ == "__main__"' in completion:
+            next_line = completion.index('if __name__ == "__main__":')
+            completion = completion[:next_line].strip()
+            # print(completion)
+
+        if "# Example usage" in completion:
+            # print(completion)
+            next_line = completion.index("# Example usage")
+            completion = completion[:next_line].strip()
+
+
         output_example = {
             'task_id': example['input']["task_id"],
-            'completion': completion.strip()
+            'completion': completion
         }
         print(output_example['completion'])
 
